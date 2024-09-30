91ccd447570253170dc8649c80067dcc9986fdeb
Ivan Hahalev <i_hahalev@ukr.net>
2024-09-30 21:29:14 +0300

    remove asyncio, change to logging, add saved response

--- main.py
+++ main.py
@@ -2,7 +2,6 @@ from datetime import datetime
 from http.server import HTTPServer, BaseHTTPRequestHandler
 from multiprocessing import Process
 from concurrent import futures as cf
-import asyncio
 import mimetypes
 import urllib.parse
 import pathlib
@@ -56,24 +55,30 @@ class HttpHandler(BaseHTTPRequestHandler):
             filename (str): requested file name
             status (int, optional): response status code. Defaults to 200.
         """
-        self.send_response(status)
-        self.send_header('Content-type', 'text/html')
-        self.end_headers()
-        with open(f'./{hppt_folder}/{filename}', 'rb') as fd:
-            self.wfile.write(fd.read())
+        try:
+            self.send_response(status)
+            self.send_header('Content-type', 'text/html')
+            self.end_headers()
+            with open(f'./{hppt_folder}/{filename}', 'rb') as fd:
+                self.wfile.write(fd.read())
+        except Exception as e:
+            logging.error(f"Unexpected error on send_html_file: {e}")
 
     def send_static(self):
         """sends requested static files like images and css
         """
-        self.send_response(200)
-        mt = mimetypes.guess_type(self.path)
-        if mt:
-            self.send_header("Content-type", mt[0])
-        else:
-            self.send_header("Content-type", 'text/plain')
-        self.end_headers()
-        with open(f'./{hppt_folder}{self.path}', 'rb') as file:
-            self.wfile.write(file.read())
+        try:
+            self.send_response(200)
+            mt = mimetypes.guess_type(self.path)
+            if mt:
+                self.send_header("Content-type", mt[0])
+            else:
+                self.send_header("Content-type", 'text/plain')
+            self.end_headers()
+            with open(f'./{hppt_folder}{self.path}', 'rb') as file:
+                self.wfile.write(file.read())
+        except Exception as e:
+            logging.error(f"Unexpected error on send_static: {e}")
 
 def run_http_server(server_class=HTTPServer, handler_class=HttpHandler):
     """configuring and running our http server
@@ -89,9 +94,36 @@ def run_http_server(server_class=HTTPServer, handler_class=HttpHandler):
     except KeyboardInterrupt:
         logging.info('Shutdown server')
     except Exception as e:
-        logging.error(f"Unexpected error on server run0: {e}")
+        logging.error(f"Unexpected error on http server run: {e}")
     finally:
         http.server_close()
+
+def save_data(data:bytes) -> dict:
+    """saves data in db
+
+    Args:
+        data (bytes): encoded data
+
+    Returns:
+        dict: decoded parsed and updated data
+    """
+    try:
+        client = MongoClient(uri, server_api=ServerApi("1"))
+        db = client.socket_db
+        data_parse = urllib.parse.unquote_plus(data.decode())
+        data_dict = {key: value for key, value in [el.split('=') for el in data_parse.split('&')]}
+        data_dict['date'] = str(datetime.now())
+        logging.info(f'Data to be written to db: {data_dict}')
+        db.messages.insert_one(data_dict)
+        return data_dict
+    except ValueError as e:
+        logging.error(f"Parsing error: {e}")
+    except Exception as e:
+        logging.error(f"Unexpected error while data saving: {e}")
+    finally:
+        if client:
+            logging.info(f"Database connection closed")
+            client.close()
 
 def send_data_to_socket(data:bytes):
     """sends data from http to socket server
@@ -103,35 +135,13 @@ def send_data_to_socket(data:bytes):
         sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         server = TCP_IP, TCP_PORT
         sock.connect(server)
-        print(f'Connection established {server}')
+        logging.info(f'Connection established {server}')
         sock.send(data)
         response = sock.recv(1024)
-        print(f'Response data: {response.decode()}')
-        print(f'Data transfer completed')
+        logging.info(f'Saved data: {response.decode()}')
+        logging.info(f'Data transfer completed')
     except Exception as e:
-        logging.error(f"Unexpected error on socket client send")
-
-async def save_data(data:bytes):
-    """saves data in db
-
-    Args:
-        data (bytes): encoded data
-    """
-    try:
-        client = MongoClient(uri, server_api=ServerApi("1"))
-        db = client.socket_db
-        data_parse = urllib.parse.unquote_plus(data.decode())
-        data_dict = {key: value for key, value in [el.split('=') for el in data_parse.split('&')]}
-        data_dict['date'] = str(datetime.now())
-        await asyncio.to_thread(db.messages.insert_one(data_dict))
-    except ValueError as e:
-        logging.error(f"Parsing error: {e}")
-    except Exception as e:
-        logging.error(f"Unexpected error while data saving: {e}")
-    finally:
-        if client:
-            logging.info(f"Database connection closed")
-            client.close()
+        logging.error(f"Unexpected error on socket client send: {e}")
 
 def handle(sock: socket.socket, address: str):
     """receives data from socket connection and calls save to db function
@@ -141,18 +151,18 @@ def handle(sock: socket.socket, address: str):
         address (str): socket client address
     """
     try:
-        print(f'Connection established {address}')
+        logging.info(f'Connection established {address}')
         while True:
             received = sock.recv(1024)
             if not received:
                 break
-            asyncio.run(save_data(received))
-            print(f'Data received: {received}')
-            sock.send(received)
-        print(f'Socket connection closed {address}')
+            logging.info(f'Data received: {received}')
+            saved = save_data(received)
+            sock.send(str(saved).encode())
     except Exception as e:
         logging.error(f"Unexpected error on socket server receive: {e}")
     finally:
+        logging.info(f'Socket connection closed {address}')
         sock.close()
 
 def run_socket_server(ip:str, port:int):
@@ -167,14 +177,16 @@ def run_socket_server(ip:str, port:int):
     sock.bind(server)
 
     sock.listen(10)
-    print(f'Start socket server {sock.getsockname()}')
+    logging.info(f'Start socket server {sock.getsockname()}')
     with cf.ThreadPoolExecutor(10) as client_pool:
         try:
             while True:
                 new_sock, address = sock.accept()
                 client_pool.submit(handle, new_sock, address)
         except KeyboardInterrupt:
-            print(f'Destroy server')
+            logging.info(f'Destroy server')
+        except Exception as e:
+            logging.error(f"Unexpected error on socket server run: {e}")
         finally:
             sock.close()
 
